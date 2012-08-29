7a83a87e92fd1e4434fc76c1583daafb032f695c
Eric Akaoka <eric.akaoka@gmail.com>
2012-08-28 22:07:30 -0400

    Created THE OBJECT.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 $(function() {
 	// do stuff when DOM is ready
+		
+	// THE OBJECT
+	
+	var traqball_1 = new Traqball({   
+	    stage:              "stage",	// id of block element. String, default value: <body>
+	    axis:               [1,0,0],	// X,Y,Z values of initial rotation vector. Array, default value: [1,0,0]
+	    angle:              0,			// Initial rotation angle in radian. Float, default value: 0.
+	    perspective:        1000,		// Perspective. Integer, default value 700.
+	    impulse:            true		// Defines if object receives an impulse after relesing mouse/touchend. Default value: true.
+	});
+
+	// THE SHELF
 	
 	$("#go").click( function() {
 		$("#shelf").toggleClass("floor_activated");
@@ -11,3 +23,6 @@ $(function() {
 	});
 	
 });		
+
+
+
